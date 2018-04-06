51c6cd50574c66ee21bfea0a6446eae4a293f9d7
Eduardo Elias Saleh <eduardo@lab15.io>
2018-04-06 21:10:03 +0200

    First commit

---
+++ TrusChain-v2/BCMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace TrusChain_v2
+{
+    public partial class BCMenu
+    {
+        private void BCMenu_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+    }
+}
